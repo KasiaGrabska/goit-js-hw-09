f0c9323ae1434298ec7f699fd2946f1a7c9b6f65
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-02-10 15:41:32 +0100

    Finished task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -17,13 +17,15 @@ const dataHours = document.querySelector('.value[data-hours]');
 const dataMinutes = document.querySelector('.value[data-minutes]');
 const dataSeconds = document.querySelector('.value[data-seconds]');
 
+let selectedDate;
+
 flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
+    selectedDate = selectedDates[0];
     if (selectedDate < new Date()) {
       window.alert('Please choose a date in the future');
       buttonStart.disabled = true;
@@ -31,4 +33,27 @@ flatpickr('#datetime-picker', {
       buttonStart.disabled = false;
     }
   },
+});
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+buttonStart.addEventListener('click', () => {
+  const today = new Date();
+  const msDifference = selectedDate - today;
+  const { days, hours, minutes, seconds } = convertMs(msDifference);
+
+  dataDays.textContent = days;
+  dataHours.textContent = hours;
+  dataMinutes.textContent = minutes;
+  dataSeconds.textContent = seconds;
 });
