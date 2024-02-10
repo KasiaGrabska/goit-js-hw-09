fbd6c136471de6916de78c98b05e840e8ae5d3e7
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-02-10 14:21:25 +0100

    Calender

---
+++ src/js/02-timer.js
@@ -0,0 +1,31 @@
+//instalacja bibilioteki flatpickr
+//funkcja flatpicker
+//onClose
+//selectedDates
+//window.alert()
+//convertMs
+//addLeadingZero(value), która używa metody padStart()
+
+import flatpickr from 'flatpickr';
+
+import 'flatpickr/dist/flatpickr.min.css';
+
+const timer = document.querySelector('.timer');
+const buttonStart = document.querySelector('button[data-start]');
+const dataDays = document.querySelector('.value[data-days]');
+const dataHours = document.querySelector('.value[data-hours]');
+const dataMinutes = document.querySelector('.value[data-minutes]');
+const dataSeconds = document.querySelector('.value[data-seconds]');
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDate[0];
+    if (selectedDate < new Date()) {
+      window.alert('Please choose a date in the future');
+    }
+  },
+});
