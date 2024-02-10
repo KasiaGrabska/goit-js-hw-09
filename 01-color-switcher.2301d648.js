d66ec690b52e335abf8b6dccfcf86b8dca67cdd1
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-02-10 11:57:03 +0000

    Deploying to gh-pages from @ KasiaGrabska/goit-js-hw-09@9ad93cd7d7c47392fb9ff8ddfb07be20aebe955f 🚀

--- 01-color-switcher.2301d648.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r;t.addEventListener("click",(()=>{r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0}));e.addEventListener("click",(()=>{r&&(clearInterval(r),t.disabled=!1)}));
-//# sourceMappingURL=01-color-switcher.2301d648.js.map
--- 01-color-switcher.be58fd17.js
+++
@@ -1,2 +0,0 @@
-!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=document.querySelector("body");e.addEventListener("click",(function(){t=setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0}));n.addEventListener("click",(function(){t&&(clearInterval(t),e.disabled=!1)}))}();
-//# sourceMappingURL=01-color-switcher.be58fd17.js.map
