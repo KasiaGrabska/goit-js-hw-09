75757d0b0a3bf5fec24b427dc5539600b4cbb365
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-02-10 12:48:08 +0100

    Finished first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+// metoda setInterval odmierza czas
+//style.backgroundcolor()
+//addEventlistener click
+//add disabled
 
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+//buttonStart.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId;
+
+const clickButtonStart = () => {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+};
+
+buttonStart.addEventListener('click', clickButtonStart);
+
+const clickButtonStop = () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    buttonStart.disabled = false;
+  }
+};
+
+buttonStop.addEventListener('click', clickButtonStop);
